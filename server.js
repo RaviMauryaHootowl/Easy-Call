4b0867aecb1ae11bb4d605224259d7fcdfc73f99
RaviMauryaHootowl <ravimaurya027@gmail.com>
2020-06-17 21:26:33 +0530

    fixed route

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ mongoose.connect('mongodb+srv://ravimaurya027:ravimaurya027@cluster0-kvlzx.mongo
 //Middlewares
 app.use(express.json());
 
-app.get('*', (req, res) => {
-  res.send('Welcome');
-})
 
 //IMPORT ROUTES
 const authRoute = require('./routes/auth');
 app.use('/api/user', authRoute);   //Middleware
 
+app.get('*', (req, res) => {
+  res.send('Welcome');
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
   console.log(`Server started at PORT ${PORT}`);
